6a4e5716cd2b62cd7617b5a56247048b058c06bf
Wellington Garcia <garciawell@gmail.com>
2019-12-09 22:23:32 -0300

    Adding scaffold widgets and icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
-// I need to import helper library
-// from flutter to get content on the screen
 import 'package:flutter/material.dart';
 
-// Define a 'main' frunction to run when our app starts
 void main() {
-// Create a new text widget to show some text
-// on the screen
   var app = MaterialApp(
-    home: Text('Hi there2!'),
-  );
+      home: Scaffold(
+    floatingActionButton: FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: () {
+        print('Hi there!');
+      },
+    ),
+    appBar: AppBar(
+      title: Text('Lets see some images!'),
+    ),
+  ));
 
-// Take that widget and get it on the screen
   runApp(app);
 }
