8af4cc141c42c1cbb0c357677a23047a9f4826c2
Wellington Garcia <garciawell@gmail.com>
2019-12-12 01:11:38 -0300

    Creating component stateful state flutter

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
         home: Scaffold(
+      body: Text('$counter'),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
+          setState(() {
+            counter += 1;
+          });
           print('Hi there!');
         },
       ),
       appBar: AppBar(
-        title: Text('Lets see some images!'),
+        title: Text('Lets see some images3!'),
       ),
     ));
   }
